95793ac0943d98b934407ac05a2fa46e1267c6fb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 10:13:44 +0300

    Imported the useColorModeValue hook

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,12 +1,14 @@
 'use client'
-import { Flex } from '@chakra-ui/react'
+import { Flex, useColorModeValue } from '@chakra-ui/react'
 
 type Props = {}
 
 const Login = (props: Props) => {
 	return (
 		<Flex height={'100vh'} align={'center'} justify={'center'}>
-			Login
+			<Flex direction={'column'} align={'center'}>
+				Main Box
+			</Flex>
 		</Flex>
 	)
 }
