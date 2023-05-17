2714b47abcfa3b1cddb50cf69c23dc1961073dcf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 10:16:03 +0300

    Styled the main box

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -6,7 +6,13 @@ type Props = {}
 const Login = (props: Props) => {
 	return (
 		<Flex height={'100vh'} align={'center'} justify={'center'}>
-			<Flex direction={'column'} align={'center'}>
+			<Flex
+				direction={'column'}
+				align={'center'}
+				background={useColorModeValue('gray.100', 'gray.800')}
+				p={'2rem'}
+				rounded={'0.5rem'}
+			>
 				Main Box
 			</Flex>
 		</Flex>
