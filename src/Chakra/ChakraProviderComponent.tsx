b0227d836b3b15af3c6256c7db9d119b1460eda9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-17 09:08:09 +0300

    Created the Chakra component

---
+++ src/Chakra/ChakraProviderComponent.tsx
@@ -0,0 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import React, { ReactNode } from 'react'
+import { theme } from './theme'
+
+type Props = {
+	children: ReactNode
+}
+
+const ChakraProviderComponent = ({ children }: Props) => {
+	return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+}
+
+export default ChakraProviderComponent
